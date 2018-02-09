93cef320c71192aacfa3ecbc8db84b957ae50943
Tomona Nanase <nanase@users.noreply.github.com>
2018-02-09 21:25:13 +0900

    First commit

---
+++ js/filter.js
@@ -0,0 +1,136 @@
+class Coefficients {
+  static normalize(coefficients) {
+    return [
+      coefficients[0] / coefficients[3],    // b0
+      coefficients[1] / coefficients[3],    // b1
+      coefficients[2] / coefficients[3],    // b2
+      -coefficients[4] / coefficients[3],   // a1
+      -coefficients[5] / coefficients[3],   // a2
+    ];
+  }
+
+  static calcOmega(sampling_rate, cutoff) {
+    return 2.0 * Math.PI * cutoff / sampling_rate;
+  }
+
+  static lowpass(sampling_rate, cutoff, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) / (2.0 * q);
+  
+    return [
+      (1.0 - Math.cos(omega)) / 2.0,  // b0
+      1.0 - Math.cos(omega),          // b1
+      (1.0 - Math.cos(omega)) / 2.0,  // b2
+      1.0 + alpha,                    // a0
+      -2.0 * Math.cos(omega),         // a1
+      1.0 - alpha,                    // a2
+    ];
+  }
+
+  static highpass(sampling_rate, cutoff, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) / (2.0 * q);
+  
+    return [
+      (1.0 + Math.cos(omega)) / 2.0,   // b0
+      -(1.0 + Math.cos(omega)),        // b1
+      (1.0 + Math.cos(omega)) / 2.0,   // b2
+      1.0 + alpha,                     // a0
+      -2.0 * Math.cos(omega),          // a1
+      1.0 - alpha,                     // a2
+    ];
+  }
+
+  static bandpass(sampling_rate, cutoff, bandwidth, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) * Math.sinh(Math.log(2.0) / 2.0 * bandwidth * omega / Math.sin(omega));
+  
+    return [
+      alpha * q,                       // b0
+      0.0,                             // b1
+      -alpha * q,                      // b2
+      1.0 + alpha,                     // a0
+      -2.0 * Math.cos(omega),          // a1
+      1.0 - alpha,                     // a2
+    ];
+  }
+
+  static bandstop(sampling_rate, cutoff, bandwidth, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) * Math.sinh(Math.log(2.0) / 2.0 * bandwidth * omega / Math.sin(omega));
+  
+    return [
+      q,                               // b0
+      -2.0 * Math.cos(omega),          // b1
+      q,                               // b2
+      1.0 + alpha,                     // a0
+      -2.0 * Math.cos(omega),          // a1
+      1.0 - alpha,                     // a2
+    ];
+  }
+
+  static lowshelf(sampling_rate, cutoff, gain, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const a = Math.pow(10.0, gain / 40.0);
+    const beta = Math.sqrt(a) / q;
+  
+    return [
+      a * (a + 1.0 - (a - 1.0) * Math.cos(omega) + beta * Math.sin(omega)),
+      2.0 * a * (a - 1.0 - (a + 1.0) * Math.cos(omega)),
+      a * (a + 1.0 - (a - 1.0) * Math.cos(omega) - beta * Math.sin(omega)),
+      a + 1.0 + (a - 1.0) * Math.cos(omega) + beta * Math.sin(omega),
+      -2.0 * (a - 1.0 + (a + 1.0) * Math.cos(omega)),
+      a + 1.0 + (a - 1.0) * Math.cos(omega) - beta * Math.sin(omega),
+    ];
+  }
+
+  static highshelf(sampling_rate, cutoff, gain, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const a = Math.pow(10.0, gain / 40.0);
+    const beta = Math.sqrt(a) / q;
+  
+    return [
+      a * (a + 1.0 + (a - 1.0) * Math.cos(omega) + beta * Math.sin(omega)),
+      -2.0 * a * (a - 1.0 + (a + 1.0) * Math.cos(omega)),
+      a * (a + 1.0 + (a - 1.0) * Math.cos(omega) - beta * Math.sin(omega)),
+      a + 1.0 - (a - 1.0) * Math.cos(omega) + beta * Math.sin(omega),
+      2.0 * (a - 1.0 - (a + 1.0) * Math.cos(omega)),
+      a + 1.0 - (a - 1.0) * Math.cos(omega) - beta * Math.sin(omega),
+    ];
+  }
+
+  static peaking(sampling_rate, cutoff, bandwidth, gain) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) * Math.sinh(Math.log(2.0) / 2.0 * bandwidth * omega / Math.sin(omega));
+    const a = Math.pow(10.0, gain / 40.0);
+  
+    return [
+      1.0 + alpha * a,          // b0
+      -2.0 * Math.cos(omega),   // b1
+      1.0 - alpha * a,          // b2
+      1.0 + alpha / a,          // a0
+      -2.0 * Math.cos(omega),   // a1
+      1.0 - alpha / a,          // a2
+    ];
+  }
+  
+  static allpass(sampling_rate, cutoff, q) {
+    const omega = this.calcOmega(sampling_rate, cutoff);
+    const alpha = Math.sin(omega) / (2.0 * q);
+  
+    return [
+      1.0 - alpha,              // b0
+      -2.0 * Math.cos(omega),   // b1
+      1.0 + alpha,              // b2
+      1.0 + alpha,              // a0
+      -2.0 * Math.cos(omega),   // a1
+      1.0 - alpha,              // a2
+    ];
+  }
+}
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,83 @@
+const diagram_assign = ['#b0', '#b1', '#b2', '#a1', '#a2'];
+const filter_controls = ['cutoff_freq', 'q', 'bandwidth', 'gain', 'sampling_rate'];
+const filters = [
+  {
+    func: p => Coefficients.lowpass(p.sampling_rate, p.cutoff, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'q'],
+  },
+  {
+    func: p => Coefficients.highpass(p.sampling_rate, p.cutoff, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'q'],
+  },
+  {
+    func: p => Coefficients.bandpass(p.sampling_rate, p.cutoff, p.bandwidth, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'bandwidth', 'q'],
+  },
+  {
+    func: p => Coefficients.bandstop(p.sampling_rate, p.cutoff, p.bandwidth, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'bandwidth', 'q'],
+  },
+  {
+    func: p => Coefficients.lowshelf(p.sampling_rate, p.cutoff, p.gain, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'gain', 'q'],
+  },
+  {
+    func: p => Coefficients.highshelf(p.sampling_rate, p.cutoff, p.gain, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'gain', 'q'],
+  },
+  {
+    func: p => Coefficients.peaking(p.sampling_rate, p.cutoff, p.bandwidth, p.gain),
+    control: ['sampling_rate', 'cutoff_freq', 'bandwidth', 'gain'],
+  },
+  {
+    func: p => Coefficients.allpass(p.sampling_rate, p.cutoff, p.q),
+    control: ['sampling_rate', 'cutoff_freq', 'q'],
+  }
+];
+
+function update_control(filter) {
+  // enable
+  filter_controls.filter(x => filter.control.some(y => x == y))
+    .forEach(c => $(`#${c}`).removeAttr('disabled').parents('.control-row').removeClass('disabled'));
+
+  // disable
+  filter_controls.filter(x => !filter.control.some(y => x == y))
+    .forEach(c => $(`#${c}`).attr('disabled', 'true').parents('.control-row').addClass('disabled'));
+}
+
+function update_filter() {
+  const filter_type = Number($('#filter_type').val());
+  const parameters = {
+    cutoff: $('#cutoff_freq').val(),
+    q: Math.pow(2, $('#q').val()),
+    bandwidth: $('#bandwidth').val(),
+    gain: $('#gain').val(),
+    sampling_rate: $('#sampling_rate').val(),
+  };
+  update_control(filters[filter_type]);
+  const result = Coefficients.normalize(filters[filter_type].func(parameters));
+  const diagram = $($('.diagram')[0].contentDocument);
+  diagram_assign.forEach((a, i) => $(a, diagram).text(`${result[i].toFixed(9)}`));
+}
+
+String.prototype.withCommas = Number.prototype.withCommas = function () { return String(this).replace(/\B(?=(\d{3})+(?!\d))/g, ","); }
+
+$(() => {
+  $('#cutoff_freq').on('input', e => $('#cutoff_freq_value').text(`${$(e.target).val().withCommas()} Hz`));
+  $('#q').on('input', e => $('#q_value').text(`${Math.pow(2, $(e.target).val()).toFixed(9)}`));
+  $('#bandwidth').on('input', e => $('#bandwidth_value').text(`${Number($(e.target).val()).toFixed(1)} oct`));
+  $('#gain').on('input', e => $('#gain_value').text(`${Number($(e.target).val()).toFixed(1)} dB`));
+  $('#sampling_rate').on('input', e => {
+    const value = $(e.target).val();
+    const max_value = ((value / 2) | 0) - 1;
+    const cutoff = $('#cutoff_freq').attr('max', max_value).val();
+    $('#sampling_rate_value').text(`${value.withCommas()} Hz`);
+
+    if (cutoff >= max_value) {
+      $('#cutoff_freq').val(max_value);
+      $('#cutoff_freq_value').text(`${max_value.withCommas()} Hz`);
+    }
+  });
+  $('.control-col input[type=range], .control-col select').on('input', update_filter);
+  $('.diagram').on('load', update_filter);
+});
